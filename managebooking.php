0d7c3b80f2e2d35836659e36a524de8f321c76ff
Matthew Cheverie <mcheveri@uwo.ca>
2023-04-05 18:29:32 -0400

    Adding manage booking

--- managebooking.php
+++ managebooking.php
@@ -133,22 +133,7 @@
       </div>
     </div>
   </div>
-  <div class="modal fade" id="successModal" tabindex="-1" aria-labelledby="successModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="successModalLabel">Success</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        Trip has been deleted successfully.
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Close</button>
-      </div>
-    </div>
-  </div>
-</div>
+
   <script>
   const deleteButtons = document.querySelectorAll('.delete-btn');
   const confirmationModal = new bootstrap.Modal(document.getElementById('confirmationModal'));
@@ -181,6 +166,21 @@ function getUrlParameter(name) {
     successModal.show();
   }
 </script>
-
+<div class="modal fade" id="successModal" tabindex="-1" aria-labelledby="successModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="successModalLabel">Success</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        Trip has been deleted successfully.
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
 
 </body>
