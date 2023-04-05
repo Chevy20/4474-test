5b2641af746a82e2bf1f56ac67ed5104b473ff77
Matthew Cheverie <mcheveri@uwo.ca>
2023-04-05 17:20:26 -0400

    Fix a.php

--- managebooking.php
+++ managebooking.php
@@ -59,6 +59,39 @@
             <div class="d-sm-flex flex-column">
                 <div>
                     <h1>Manage Boookings</h1>
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Booking #</th>
+                                <th scope="col">User ID</th>
+                                <th scope="col">Trip ID</th>
+                                <th scope="col">Trip Name</th>
+                                <th scope="col">Start Date</th>
+                                <th scope="col">End Date</th>
+                                <th scope="col">Cost</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                                $result1 = $connection->query("SELECT booking_num, user_id, trip_id, trip_name, startDate, endDate, Cost FROM Travelled_trip WHERE user_id = $user_id" );
+                                $tripper = 1;
+                                while ($row1 = $result1->fetch_assoc()) {
+                                    echo '<tr>';
+                                    echo '<th scope="row">' . $tripper . '</th>';
+                                    echo '<td>' . $row1["booking_num"] . '</td>';
+                                    echo '<td>' . $row1["user_id"] . '</td>';
+                                    echo '<td>' . $row1["trip_id"] . '</td>';
+                                    echo '<td>' . $row1["trip_name"] . '</td>';
+                                    echo '<td>' . $row1["startDate"] . '</td>';
+                                    echo '<td>' . $row1["endDate"] . '</td>';
+                                    echo '<td>' . $row1["Cost"] . '</td>';
+                                    echo '</tr>';
+                                    $tripper = $tripper + 1;
+                                }
+                            ?>
+                        </tbody>
+                    </table>
                 </div>
 
 
